2f5999fd6305127b02ad9a747c9e56b9aff699c4
404xNOTFOUND <cadetlax@gmail.com>
2025-05-08 14:14:43 -0400

    Finished game.

--- main.py
+++ main.py
@@ -1,36 +1,67 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+import sys
 import pygame
 from constants import * 
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
-    dt = 0
-
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+   
+    dt =0
+   
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+       
+        
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split() 
         
         screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
+        pygame.display.flip()
         player.draw(screen)
+
         
         dt = clock.tick(60) / 1000
-        player.update(dt)
-        pygame.display.flip()
+        # player.update(dt)
+        # updatable.update(dt)
 
-        # dt = clock.tick(60) / 1000
+        # pygame.display.flip()
 
-        # player.draw(screen)
+        
 
     print ("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
